886dffaa84e62e9c474f9631be10062981665cfd
Anders Pearson <anders@columbia.edu>
2015-12-14 10:42:22 +0000

    release 0.1.1

---
+++ CHANGES.md
@@ -0,0 +1,4 @@
+0.1.1 (2015-12-14)
+==================
+
+* fix path bug with s3 static files (non-cloudfront)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ccnmtlsettings",
-    version="0.1.0",
+    version="0.1.1",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="https://github.com/ccnmtl/ccnmtlsettings",
