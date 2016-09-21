8e2d3554fcf4e67f9d30e6a7397990e1716a0c1c
Anders Pearson <anders@columbia.edu>
2016-09-21 12:08:46 +0100

    add docker and docker-compose settings
    a starting point for making it a bit simpler to run our apps with docker/docker-compose

---
+++ ccnmtlsettings/compose.py
@@ -0,0 +1,21 @@
+# docker-compose settings doesn't really have to do much
+# since it's a dev environment and is mostly covered by
+# settings_shared. mostly, it just has to set up some
+# defaults
+import os
+import sys
+
+
+def common(**kwargs):
+    DEBUG = True
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'HOST': 'db',
+            'PORT': 5432,
+            'ATOMIC_REQUESTS': True,
+        }
+    EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+    return locals()
