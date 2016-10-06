f99df329d600a04980173400212a938239d2bf1f
Susan Dreher <sdreher@columbia.edu>
2016-10-06 11:51:44 -0400

    bumping version number to 1.1.4 removing TEMPLATE_DEBUG

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ccnmtlsettings",
-    version="1.1.2",
+    version="1.1.4",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="https://github.com/ccnmtl/ccnmtlsettings",
