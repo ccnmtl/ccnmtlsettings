a7ddaf9fa043bf1d290cb6f6d648abd33b0f16e2
Susan Dreher <sdreher@gmail.com>
2019-11-22 14:34:04 -0500

    Release prep for 1.7.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ccnmtlsettings",
-    version="1.6.0",
+    version="1.7.0",
     author="Anders Pearson",
     author_email="ctl-dev@columbia.edu",
     url="https://github.com/ccnmtl/ccnmtlsettings",
