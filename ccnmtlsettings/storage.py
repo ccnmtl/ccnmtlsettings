f1c0321b6d724b29b18d694838626fba5cfa582a
Susan Dreher <sdreher@gmail.com>
2020-05-30 13:24:07 -0400

    restore previous naming convention

--- ccnmtlsettings/storage.py
+++ ccnmtlsettings/storage.py
@@ -1,5 +1,9 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class UploadsS3Boto3Storage(S3Boto3Storage):
+class CompressorS3Boto3Storage(S3Boto3Storage):
+    location = 'media'
+
+
+class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = 'uploads'
