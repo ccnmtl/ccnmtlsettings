9cbb152efeec2d04eee218944810989e001c892d
Nikolas Nyby <nikolas@gnu.org>
2020-07-16 15:33:36 -0400

    Use cacheds3storage
    Use the package we created here, again:
    https://github.com/ccnmtl/django-cacheds3storage
    
    This should add back in integration between django-compressor and
    django-storages. I realized this while reading django-compressor's docs
    here:
    https://django-compressor.readthedocs.io/en/stable/remote-storages/#using-staticfiles
    
    This got lost during the django-storages / boto3 update, when I reverted
    to django-storage's default classes for this. But, our cacheds3storage
    is now compatible with the new boto3, so this should work.

--- ccnmtlsettings/storage.py
+++
@@ -1,9 +0,0 @@
-from storages.backends.s3boto3 import S3Boto3Storage
-
-
-class CompressorS3Boto3Storage(S3Boto3Storage):
-    location = 'media'
-
-
-class MediaRootS3Boto3Storage(S3Boto3Storage):
-    location = 'uploads'
