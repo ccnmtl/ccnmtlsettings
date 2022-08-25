3908394f57de058d327e3d2600261339e85134f1
Nikolas Nyby <nikolas@gnu.org>
2022-08-25 16:17:35 -0400

    Decrease file logging level to INFO

--- ccnmtlsettings/staging.py
+++ ccnmtlsettings/staging.py
@@ -69,7 +69,7 @@ def common(**kwargs):
         'disable_existing_loggers': False,
         'handlers': {
             'file': {
-                'level': 'DEBUG',
+                'level': 'INFO',
                 'class': 'logging.FileHandler',
                 'filename': '/var/log/django/' + project + '/debug.log',
             },
@@ -77,7 +77,7 @@ def common(**kwargs):
         'loggers': {
             'django': {
                 'handlers': ['file'],
-                'level': 'DEBUG',
+                'level': 'INFO',
                 'propagate': True,
             },
         },
