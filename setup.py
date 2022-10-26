5cfec206a630c886757ce1fd7253d120460876e3
Nikolas Nyby <nikolas@gnu.org>
2022-10-26 12:35:23 -0400

    Remove django-waffle from common settings
    django-waffle is a feature flipper that we only rarely use these days.
    Feature flags can be useful, but they also can introduce complexity to
    an application.
    
    In any case, I think it's fine to just manually include django-waffle in
    the case that we do want to use a feature flag. It definitely doesn't need
    to be included in all our projects.

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
     install_requires = [
         "django_compressor",
         "django-debug-toolbar",
-        "django-waffle",
         "coverage",
         "django-smoketest",
         "django-extensions",
